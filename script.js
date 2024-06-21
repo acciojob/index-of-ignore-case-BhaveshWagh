c2f58cec3bb8cc493e7231d955f55971ecd75335
Bhavesh Wagh <bhaveshbm2711@gmail.com>
2024-06-21 10:51:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-function indexOfIgnoreCase(s1, s2) {
+function indexOfIgnoreCase(str, subStr) {
   // write your code here
+	const strLower = str.toLowerCase();
+    const subStrLower = subStr.toLowerCase();
+    
+    if (subStrLower === "") {
+        return 0; // Empty substring is always found at index 0
+    }
+    
+    for (let i = 0; i <= str.length - subStr.length; i++) {
+        if (strLower.substring(i, i + subStr.length) === subStrLower) {
+            return i;
+        }
+    }
+    
+    return -1; // substring not found
 }
 
 // Please do not change the code below
